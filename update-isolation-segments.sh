f37f504f39ff0bf04d631207a14d639b6c0ee8e7
Chris Nelson <christopher.nelson@gsa.gov>
2017-06-06 14:26:39 -0700

    typo fix

--- update-isolation-segments.sh
+++ update-isolation-segments.sh
@@ -8,5 +8,5 @@ cf api "${CF_API_URL}"
 cf target -o "${PLATFORM_ORGANIZATION}"
 
 cf create-isolation-segment "${PLATFORM_ISOLATION_SEGMENT}"
-cf enable-org-isolation "${PLATFORM_ORG}" "${PLATFORM_ISOLATION_SEGMENT}"
+cf enable-org-isolation "${PLATFORM_ORGANIZATION}" "${PLATFORM_ISOLATION_SEGMENT}"
 cf set-space-isolation-segment "${PLATFORM_SPACE}" "${PLATFORM_ISOLATION_SEGMENT}"
